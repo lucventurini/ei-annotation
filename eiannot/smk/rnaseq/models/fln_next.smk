75b0634cffea41cacdd3372e8c74bb106447edaa
Luca Venturini <Luca.Venturini@tgac.ac.uk>
2018-02-21 17:33:34 +0000

    The FLN pipeline is semi-functional.

--- eiannot/smk/rnaseq/models/fln_next.smk
+++ eiannot/smk/rnaseq/models/fln_next.smk
@@ -3,70 +3,92 @@ import subprocess
 from eicore.external_process.snakemake_helper import loadPreCmd
 
 
-rule extract_mikado_fasta:
-  # A problem here is that we should select ONE mikado output. Instead Daijin returns multiple
-  input:
-    mikado=os.path.join(outdir, "Daijin", "5-mikado", "pick", "permissive", "mikado-permissive.loci.gff3"),
-    genome=REF
-  output: os.path.join(outdir, "Daijin", "5-mikado", "pick", "permissive", "mikado-permissive.loci.cds.fasta")
-  params:
-    load=loadPreCmd(config["load"]["gffread"])
-  shell: """{params.load} gffread -g {input.ref} -w {output} {input.mikado}"""
-
-rule convert_mikado_to_bed12:
-  input:
-    os.path.join(outdir, "Daijin", "5-mikado", "pick", "permissive", "mikado-permissive.loci.gff3"),
-  output: os.path.join(outdir, "Daijin", "5-mikado", "pick", "permissive", "mikado-permissive.loci.bed12")
-  params:
-    load=loadPreCmd(config["load"]["mikado"])
-  shell: """{params.load} mikado util convert -of bed12 {input.mikado} {output}"""
-
-
 rule fln_all:
 	input: expand(os.path.join(outdir, "Hints", "FLN", "Chunks", "chunk-{chunk_id}", "fln_results", "pt_seqs"), chunk_id=CHUNK_ARRAY)
 	output:
 	  flag=os.path.join(outdir, "Hints", "FLN", "fln.done"),
 	  table=os.path.join(outdir, "Hints", "FLN", "fln.table.txt")
-	shell: "cat {input} | sed '2,/^Query_id/d' > {output.table} && touch {output.flag}"
+	shell: """cat {input} | awk 'NR==1 || $1!="Query_id"'   > {output.table} && touch {output.flag}"""
 
 
 rule filter_fln:
   input:
     table=rules.fln_all.output.table,
     bed12=rules.convert_mikado_to_bed12.output
-  output: os.path.join(outdir, "Hints", "FLN", "fln.filtered_table.txt")
+  output:
+    table=os.path.join(outdir, "Hints", "FLN", "fln_filtered.table.txt"),
+    list=os.path.join(outdir, "Hints", "FLN", "fln_filtered.list.txt")
   params:
-    load=loadPreCmd(config["load"]["mikado"])
-  shell: """{params.load}  """
+    load=loadPreCmd(config.get("load", {}).get("mikado", None), config.get("load", {}).get("ei-annot", None)),
+    out=os.path.join(outdir, "Hints", "FLN", "fln_filtered"),
+    mikado=os.path.join(outdir, "Daijin", "5-mikado", "pick", "permissive", "mikado-permissive.loci")
+  shell: """{params.load} filter_fln.py {input.table} {params.mikado} {params.out}"""
+
+rule extract_gold_gff:
+  input:
+    list=rules.filter_fln.output.list,
+    mikado=rules.index_mikado.input.mikado
+  output:
+    gff3=os.path.join(outdir, "Hints", "FLN", "gold_models.gff3"),
+    list=os.path.join(outdir, "Hints", "FLN", "gold_models.txt")
+  params:
+    load=loadPreCmd(config.get("load", {}).get("mikado", None))
+  shell: """{params.load} awk '{{if ($4=="Gold") {{OFS="\\t"; print $1,$2}}}}' {input.list} > {output.list} &&
+  mikado util grep {output.list} {input.mikado} {output.gff3}"""
+
+rule extract_silver_gff:
+  input:
+    list=rules.filter_fln.output.list,
+    mikado=rules.index_mikado.input.mikado
+  output:
+    gff3=os.path.join(outdir, "Hints", "FLN", "silver_models.gff3"),
+    list=os.path.join(outdir, "Hints", "FLN", "silver_models.txt")
+  params:
+    load=loadPreCmd(config.get("load", {}).get("mikado", None))
+  shell: """{params.load} awk '{{if ($4=="Silver") {{OFS="\\t"; print $1,$2}}}}' {input.list} > {output.list} &&
+  mikado util grep {output.list} {input.mikado} {output.gff3}"""
 
 
+rule extract_bronze_gff:
+  input:
+    list=rules.filter_fln.output.list,
+    mikado=rules.index_mikado.input.mikado
+  output:
+    gff3=os.path.join(outdir, "Hints", "FLN", "bronze_models.gff3"),
+    list=os.path.join(outdir, "Hints", "FLN", "bronze_models.txt")
+  params:
+    load=loadPreCmd(config.get("load", {}).get("mikado", None))
+  shell: """{params.load} awk '{{if ($4=="Bronze") {{OFS="\\t"; print $1,$2}}}}' {input.list} > {output.list} &&
+  mikado util grep {output.list} {input.mikado} {output.gff3}"""
 
 
-rule split_mikado:
-	input: tr=rules.extract_mikado_fasta.output
-	output: os.path.join(outdir, "Hints", "FLN", "fastas", "split.done")
-	params:
-		outdir=os.path.join(outdir, "Hints", "FLN", "fastas"),
-		chunks=config["fln"]["chunks"],
-		load=loadPreCmd(config["load"]["mikado"])
-	threads: 1
-	message: "Splitting fasta: {input.tr}"
-	shell: """{params.load} mkdir -p {params.outdir} &&
-	split_fasta.py -m {params.chunks} {input.tr} {params.outdir} && touch {output}"""
+rule extract_training_gff:
+  input:
+    list=rules.filter_fln.output.list,
+    mikado=rules.index_mikado.input.mikado
+  output:
+    gff3=os.path.join(outdir, "Hints", "FLN", "training_models.gff3"),
+    list=os.path.join(outdir, "Hints", "FLN", "training_models.txt")
+  params:
+    load=loadPreCmd(config.get("load", {}).get("mikado", None))
+  shell: """{params.load} awk '{{if ($3=="True") {{OFS="\\t"; print $1,$2}}}}' {input.list} > {output.list} &&
+  mikado util grep {output.list} {input.mikado} {output.gff3}"""
+
 
 rule fln:
   input:
-    split=rules.split_fa.output
+    split=rules.split_mikado.output
   output:
-    ptseq=os.path.join(outdir, "Hints", "FLN", "Chunks", "chunk-{chunk_id}", "fln_results", "pt_seqs"),
-    flag=os.path.join(outdir, "Hints", "FLN", "Chunks", "chunk-{chunk_id}", "fln.done"),
+    ptseq=touch(os.path.join(outdir, "Hints", "FLN", "Chunks", "chunk-{chunk_id}", "fln_results", "pt_seqs")),
+    flag=touch(os.path.join(outdir, "Hints", "FLN", "Chunks", "chunk-{chunk_id}", "fln.done"))
   params:
     taxon=config["fln"]["taxon"],
+    dbs=config["fln"]["dbs"],
+    fasta=lambda wildcards: os.path.join(outdir, "Hints", "FLN", "fastas", "chunk_%s.fasta" % wildcards.chunk_id),
     outdir=os.path.join(outdir, "Hints", "FLN", "Chunks", "chunk-{chunk_id}"),
-    load=loadPreCmd(config["load"]["full_lengther_next"]),
+    load=loadPreCmd(config.get("load", dict()).get("full_lengther_next", None)),
   threads: THREADS
   log: os.path.join(outdir, "Hints", "FLN", "Chunks", "chunk-{chunk_id}", "fln.log")
-  shell: """{params.load} mkdir -p {params.outdir} && ln -rst {params.outdir} {input} &&
-  cd {params.outdir} && mv $(basename {input}) chunk.fasta &&
-  full_lengther_next -w {threads} -g {params.taxon} -f chunks.fasta 2>&1 > fln.log &&
-  touch {params.outdir}/fln.done"""
+  shell: """{params.load} mkdir -p {params.outdir} && ln -rst {params.outdir} {params.fasta} &&
+  cd {params.outdir} && mv $(basename {params.fasta}) chunk.fasta &&
+  full_lengther_next -w {threads} -g {params.taxon} -a {params.dbs} -f chunk.fasta 2>&1 > fln.log"""
--- eiannot/util/filter_fln.py
+++ eiannot/util/filter_fln.py
@@ -1,36 +1,162 @@
 #!/usr/bin/env python3
 
 import sys
+import os
 import argparse
 import pandas as pd
 import Mikado
 import itertools
+import sqlite3
+import collections
+import magic
+from Mikado.exceptions import CorruptIndex
+from functools import partial
+
+
+def build_pos_index(index_name):
+
+    positions = dict()
+
+    wizard = magic.Magic(mime=True)
+
+    if wizard.from_file("{0}".format(index_name)) == b"application/gzip":
+        raise CorruptIndex("Invalid index file")
+    try:
+        conn = sqlite3.connect("{0}".format(index_name))
+        cursor = conn.cursor()
+        tables = cursor.execute("SELECT name FROM sqlite_master WHERE type='table';").fetchall()
+        if sorted(tables) != sorted([("positions",), ("genes",)]):
+            raise CorruptIndex("Invalid database file")
+    except sqlite3.DatabaseError:
+        raise CorruptIndex("Invalid database file")
+
+    genes = dict()
+    try:
+        for counter, obj in enumerate(cursor.execute("SELECT * from positions")):
+            chrom, start, end, gid = obj
+            if chrom not in positions:
+                positions[chrom] = collections.defaultdict(list)
+            positions[chrom][(start, end)].append(gid)
+            genes[gid] = (chrom, start, end)
+    except sqlite3.DatabaseError:
+        raise CorruptIndex("Invalid index file. Rebuilding.")
+
+    indexer = collections.defaultdict(list).fromkeys(positions)
+    for chrom in positions:
+        indexer[chrom] = Mikado.utilities.intervaltree.IntervalTree.from_tuples(
+            positions[chrom].keys()
+        )
+
+    return indexer, positions, genes
+
+
+def remove_genes_with_overlaps(training_candidates: pd.DataFrame,
+                               indexer: dict, positions: dict, genes: dict, flank: int):
+
+    to_remove = set()
+
+    for num in range(training_candidates.shape[0]):
+        gid = training_candidates["parent"].iloc[num]  # Get the gene name
+        found = set()
+        chrom, start, end = genes[gid]
+        for interval in indexer[chrom].find(start-flank, end+flank):
+            found = set.union(found, positions[chrom][(interval[0], interval[1])])
+        found.remove(gid)
+        if len(found) > 0:
+            to_remove.add(gid)
+    training_candidates = training_candidates[~
+                              training_candidates[training_candidates.columns[0]].isin(to_remove)]
+    return training_candidates
+
+
+def determine_category(row, gold, silver, bronze):
+
+    if row[0] in gold:
+        return "Gold"
+    elif row[0] in silver:
+        return "Silver"
+    elif row[0] in bronze:
+        return "Bronze"
+    else:
+        return "NA"
 
 
 def main():
 
     parser = argparse.ArgumentParser(__doc__)
+    parser.add_argument("--flank", type=int, default=1000)
     parser.add_argument("fln")
-    parser.add_argument("bed12")
+    parser.add_argument("mikado")
     parser.add_argument("out")
     args = parser.parse_args()
 
-    fln = pd.read_csv(args.fln, sep="\t")
+    args.flank = abs(args.flank)
+    bed12 = args.mikado + ".bed12"
+    metrics = args.mikado + ".metrics.tsv"
+    midx = args.mikado + ".gff3.midx"
 
-    complete = fln[(fln.Status.str.match("complete", case=False)) & (~fln.Status.str.match("putative", case=False))]
-    transcripts = [Mikado.transcripts.Transcript(_) for _ in Mikado.parsers.bed12.Bed12Parser(args.bed12)
+    fln = pd.read_csv(args.fln, sep="\t")
+    transcripts = [Mikado.transcripts.Transcript(_) for _ in Mikado.parsers.bed12.Bed12Parser(bed12)
                    if _.header is False]
     orfs = list(itertools.chain(
         *[[(t.id, _.thick_start, _.thick_end) for _ in t.get_internal_orf_beds()] for t in transcripts]))
-    orf_df = pd.DataFrame(orfs, columns=[complete.columns[0], "mikado_orf_start", "mikado_orf_end"])
+    orf_df = pd.DataFrame(orfs, columns=[fln.columns[0], "mikado_orf_start", "mikado_orf_end"])
+    metrics = pd.read_csv(metrics, sep="\t")
+    metrics["transcripts_per_gene"] = metrics.groupby("parent")["parent"].transform("count")
+    merged = pd.merge(pd.merge(fln, orf_df, on=fln.columns[0]),
+                              
+                              metrics, left_on=fln.columns[0], right_on=metrics.columns[0])
 
-    merged = pd.merge(complete, orf_df, on=complete.columns[0])
-    correct = merged[
-        (merged["ORF_start"] == merged["mikado_orf_start"]) & (merged["ORF_end"] == merged["mikado_orf_end"])]
+    print(orf_df.shape, metrics.shape, fln.shape, merged.shape)
 
-    assert isinstance(correct, pd.DataFrame)
-    correct.to_csv(path_or_buf=args.out, sep="\t", header=True, index=False, index_label=False)
+    # Now that we have a complete table, it is easy to filter out
 
+    training_candidates = merged[(
+        (merged.Status.str.match("complete", case=False)) &
+        (merged["ORF_start"] == merged["mikado_orf_start"]) & (merged["ORF_end"] == merged["mikado_orf_end"]) &
+        ((merged.combined_cds_length == merged.selected_cds_length) & 
+         (merged.five_utr_num > 0) & (merged.three_utr_num > 0) & (merged.combined_cds_fraction >= 0.5) &
+         (merged.transcripts_per_gene == 1) & (merged.source_score == merged.source_score.max()))
+    )][[merged.columns[0], "parent"]]
+
+    # Now we have to exclude genes that are within 1000bps of another gene
+    # Load from the MIDX the positions
+
+    indexer, positions, genes = build_pos_index(midx)
+    training_candidates = remove_genes_with_overlaps(training_candidates, indexer, positions, genes, args.flank)
+
+    # This instead are the gold/silver/bronze categories for the training
+
+    gold = merged[(
+        (merged.Status.str.match("complete", case=False)) &
+        ((merged["ORF_start"] == merged["mikado_orf_start"]) & (merged["ORF_end"] == merged["mikado_orf_end"])) &
+        ((merged.combined_cds_length == merged.selected_cds_length) & (merged.selected_cds_length>=300) &
+         (merged.five_utr_num == 2) & (merged.three_utr_num == 1))
+    )][[merged.columns[0], "parent"]]
+
+    silver = merged[(
+        (~merged["parent"].isin(gold["parent"])) &
+        ((merged.combined_cds_length == merged.selected_cds_length) & (merged.selected_cds_length>=300) &
+         (merged.five_utr_num == 2) & (merged.three_utr_num == 1))
+    )][[merged.columns[0], "parent"]]
+
+    bronze = merged[((~merged["parent"].isin(silver["parent"])) &
+                     (~merged["parent"].isin(gold["parent"])) &
+                     (merged["max_intron_length"] <= 50000))][[merged.columns[0], "parent"]]
+
+    merged["Training"] = merged[merged.columns[0]].isin(training_candidates[training_candidates.columns[0]])
+    cat = partial(determine_category,
+                  gold=set(gold.iloc[:, 0].unique()),
+                  silver=set(silver.iloc[:, 0].unique()),
+                  bronze=set(bronze.iloc[:, 0].unique()))
+
+    merged["Category"] = merged.apply(cat, axis=1)
+
+    # Now write out the CSVs ...
+    merged.to_csv(args.out + ".table.txt", sep="\t", index=False, header=True)
+    merged[[merged.columns[0], "parent", "Training", "Category"]].to_csv(
+        args.out + ".list.txt", sep="\t", index=False, header=True
+    )
     return
 
 
