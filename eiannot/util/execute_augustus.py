4380f56b471a61768bac49aac9ca9b49155e5332
Luca Venturini <lucventurini@gmail.com>
2018-10-29 16:59:17 +0000

    #11: chunk splitting algorithm done.

--- eiannot/util/execute_augustus.py
+++ eiannot/util/execute_augustus.py
@@ -4,3 +4,62 @@
 from eiannot.library.chromclass import Chrom, Chunk, AugBase
 from sqlalchemy.engine import create_engine, Engine
 from sqlalchemy.orm.session import Session
+import argparse
+import os
+import subprocess
+import Bio.SeqIO
+import Bio.bgzf
+from functools import partial
+import tempfile
+
+
+__doc__ = """"""
+
+
+def positive(string):
+
+    string = int(string)
+    if string < 1:
+        raise ValueError("Only positive values are acceptable!")
+    return string
+
+
+def main():
+
+    fasta_index = partial(Bio.SeqIO.index, format="fasta")
+
+    parser = argparse.ArgumentParser(__doc__)
+    parser.add_argument("genome", type=fasta_index)
+    parser.add_argument("db")
+    parser.add_argument("chunk", type=positive)
+    parser.add_argument("out", type=argparse.FileType("wt"))
+    parser.add_argument("augustus")
+    args = parser.parse_args()
+
+    assert os.path.exists(args.db)
+    engine = create_engine("sqlite:///{args.db}".format(**locals()))
+    session = Session(bind=engine, autocommit=True, autoflush=True)
+    # Check that the database is initialised properly
+    assert set.intersection(set(engine.table_names()), set(AugBase.metadata.tables.keys())) == set(engine.table_names())
+    # Now query the chunk ID
+
+    for chunk in session.query(Chunk).filter(Chunk.chunk_id == args.chunk):
+        chrom_file = Bio.bgzf.open(tempfile.mktemp(suffix=".fa.gz", dir=os.getcwd()), mode="wt")
+        chrom, start, end = chunk.chrom, chunk.start, chunk.end
+        # Using SeqIO because it is much faster than
+        Bio.SeqIO.write(args.genome[chrom], chrom_file, "fasta")
+        chrom_file.flush()
+        assert os.path.exists(chrom_file._handle.name)
+        assert os.stat(chrom_file._handle.name).st_size > 0
+        # We will have to use bgzip to decompress on the fly
+        command = "{args.augustus} --predictionStart={start} --predictionEnd={end} "
+        command += "<(bgzip -cd {chrom_file._handle.name})"
+        command = command.format(**locals())
+        print(command)
+        subprocess.call(command, shell=True, stdout=args.out, executable="/bin/bash")
+        os.remove(chrom_file._handle.name)
+
+    return
+
+
+main()
--- eiannot/util/split_genome_fasta.py
+++ eiannot/util/split_genome_fasta.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 
-from itertools import chain, repeat
 import argparse
 import os
 import pyfaidx
-from itertools import zip_longest
 from eiannot.library.chromclass import Chrom, Chunk, AugBase
 from sqlalchemy.engine import create_engine, Engine
 from sqlalchemy.orm.session import Session
 from collections import defaultdict
+import numpy as np
 
 
 __doc__ = """Script to split FASTA sequences in a fixed number of multiple files.
@@ -22,11 +21,6 @@ def positive(string):
     if string < 1:
         raise ValueError("Only positive values are acceptable!")
     return string
-
-
-def fixed_grouper(number, iterable, padvalue=None):
-    # return [iterable[x:x + n] for x in range(0, len(MyList), n)]
-    return zip_longest(*[chain(iterable, repeat(padvalue, number - 1))] * number)
 
 
 def percentage(string):
@@ -47,8 +41,12 @@ def main():
     parser = argparse.ArgumentParser(__doc__)
     parser.add_argument("-n", "--num-chunks", required=True, type=positive, dest="num_chunks")
     parser.add_argument("-ms", "--minsize", required=True, type=positive)
+    parser.add_argument("-minOP", required=False, default=0.1, type=percentage,
+                        help="Maximum overlap in percentage of the chunk. Default: 10%.")
+    parser.add_argument("-maxOP", required=False, default=0.7, type=percentage,
+                        help="Maximum overlap in percentage of the chunk. Default: 70%.")
     parser.add_argument("-minO", "--min-overlap",
-                        required=True,
+                        required=True, help="Minimum overlap length.",
                         type=positive, dest="min_overlap")
     # parser.add_argument("-o", "--overlap", required=True, type=percentage)
     parser.add_argument("fasta", type=pyfaidx.Fasta, help="Input FASTA file.")
@@ -56,8 +54,10 @@ def main():
     args = parser.parse_args()
 
     args.overlap = round(args.min_overlap * 1.0 / args.minsize, 2)
-    if args.overlap >= 1:
-        raise ValueError("The overlap cannot be larger than the minimum size. Aborting.")
+    if args.overlap >= args.maxOP:
+        # raise ValueError("The overlap cannot be larger than the minimum size. Changing.")
+        # This is a mistake. Correcting.
+        args.minsize = args.min_overlap / args.maxOP
 
     # Create the tables
     if os.path.exists(args.db):
@@ -76,11 +76,6 @@ def main():
         total_length += l
     session.bulk_save_objects(chroms)
 
-    chrom_cache = dict()
-
-    for obj in session.query(Chrom):
-        chrom_cache[obj.name] = obj.chrom_id
-
     # Now we have to determine the chunks. Importantly, there can be *less* chunks than the number specified.
     # L = w * ( n - n*o + o )
     # L: total length
@@ -89,26 +84,73 @@ def main():
     # w: chunk width
     # => w = L / (n - n*o + o)
 
-    chunk_size = 0
-    overlap = 0
+    total = 0
+    min_size = args.minsize * 2 - args.min_overlap
 
-    while chunk_size < args.minsize or overlap < args.min_overlap:
-        if args.overlap >= 1:
-            raise ValueError("It is impossible to create a chunk distribution with the required parameters. Aborting")
+    chroms = [chrom for chrom in session.query(Chrom).order_by(Chrom.length.desc())]
 
-        chunk_size = int(total_length / (args.num_chunks - args.num_chunks * args.overlap + args.overlap))
-        overlap = int(round(chunk_size * args.overlap, 0))
-        args.overlap += 0.01
+    chunk_size = args.minsize
+    rm = args.num_chunks
 
+    correct = set()
+
+    while chunk_size < total_length / (args.num_chunks + args.maxOP - args.num_chunks * args.maxOP):
+        chunk_size = max(chunk_size + 1, int(round(chunk_size * 1.01)))
+        remainder = 0
+        current_chunk = 0
+        _exhausted_chunks = 0
+        for chrom in chroms:
+            if chrom.length > min_size:
+                if current_chunk > 0:
+                    remainder += current_chunk
+                    _exhausted_chunks += 1
+                    current_chunk = 0
+                continue
+            else:
+                remainder += chrom.length
+                current_chunk += chrom.length
+                if current_chunk >= min_size:
+                    current_chunk = 0
+                    _exhausted_chunks += 1
+        rm = remaining_chunks = args.num_chunks - _exhausted_chunks
+        # If the overlap is the minimum, then the covered distance is maximum
+        max_total = remainder + chunk_size * (rm - rm * args.minOP - args.minOP)
+        if max_total < total_length:
+            # print(remainder, chunk_size, args.maxOP, max_total, total_length)
+            continue  # We will *never* reach the proper size
+        else:
+            # Now the name of the game is to find the minimum overlap which,
+            # given this chunk size, will be bound and be above the total
+            diff = float("inf")
+            for ol in np.arange(args.minOP, args.maxOP, 0.01):
+                if ol * chunk_size < args.min_overlap:
+                    continue
+                total = remainder + chunk_size * (rm - rm * ol - ol)
+                if abs(total_length - total) / total_length <= 0.05:
+                    correct.add((chunk_size, ol))
+
+    if len(correct) == 0:
+        # Nothing has been found that respects the parameters! Aborting
+        raise OSError(
+            """The parameters inputed for the program are unsatisfiable;
+            I cannot find a suitable way to partition the genome. Please increase the maximum overlap or
+            reduce the number of chunks.""")
+
+    correct = {chunk_size * ol: (chunk_size, ol) for chunk_size, ol in correct}
+    chunk_size, args.overlap = correct[min(correct.keys())]
+
+    # print(rm, remainder, overlap/chunk_size, total, chunk_size, overlap)
     assert chunk_size >= args.minsize
-    assert overlap >= args.min_overlap
+    assert args.minOP <= args.overlap <= args.maxOP, args.overlap
+    overlap = int(round(chunk_size * args.overlap))
+    assert overlap >= args.min_overlap, (chunk_size, args.overlap, overlap, args.min_overlap)
 
     chunks = defaultdict(list)
     chunk_id = 0
     chunks = []
     current_chunk = []
 
-    for chrom in args.fasta.keys():
+    for chrom in chroms:
         # We might have more than one chunk here, depending on the size of the chromosome.
         # In short, we want the following:
         # - cluster up the end of the chromosomes if we have less than the overlap remaining (thus reducing the
@@ -118,15 +160,16 @@ def main():
         # - otherwise, just store the overlap and continue
         start = 0
         end = 0
-        print("Starting with", chrom)
-        while end < len(args.fasta[chrom]):
+
+        while end < chrom.length:
             end = start + chunk_size
-            if end > len(args.fasta[chrom]):
-                current_chunk.append((chunk_id, chrom_cache[chrom], start, len(args.fasta[chrom])))
+            if end > chrom.length:
+                current_chunk.append((chunk_id, chrom.chrom_id, start, chrom.length))
                 break
-            if end + overlap > len(args.fasta[chrom]):
-                end = len(args.fasta[chrom])
-            current_chunk.append((chunk_id, chrom_cache[chrom], start, end))
+            if end + chunk_size - overlap > chrom.length or (start == 0 and
+                                                             chunk_size * 2 - overlap >= chrom.length):
+                end = chrom.length
+            current_chunk.append((chunk_id, chrom.chrom_id, start, end))
             chunk_id += 1
             chunks.extend(current_chunk)
             current_chunk = []
